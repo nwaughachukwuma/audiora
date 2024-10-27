3bb30709bd951b063c9217f30bf91dc23e565a7b
Chukwuma Nwaugha <nwaughac@gmail.com>
2024-10-27 15:14:36 +0000

    move handle_selected_example to utils/chat_thread

--- src/utils/example_utils.py
+++ src/utils/example_utils.py
@@ -1,10 +1,6 @@
 import streamlit as st
 
-from src.utils.chat_response import generate_stream_response
-from src.utils.chat_utils import (
-    ContentType,
-    content_examples,
-)
+from src.utils.chat_utils import content_examples
 
 
 def display_example_cards():
@@ -41,21 +37,3 @@ def display_example_cards():
                 st.session_state.seleted_example = example
 
                 st.rerun()
-
-
-def handle_selected_example(content_type: ContentType):
-    """Handle selected example prompt"""
-    prompt = st.session_state.seleted_example
-
-    with st.chat_message("assistant"):
-        response_generator = generate_stream_response(prompt, content_type)
-        ai_message = st.write_stream(response_generator)
-        st.session_state.seleted_example = None
-
-        if ai_message:
-            st.session_state.messages.append(
-                {"role": "assistant", "content": ai_message}
-            )
-            st.rerun()
-        else:
-            st.error("Failed to generate AI response. Please try again.")
