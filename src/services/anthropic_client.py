4156c2b4aa59fb7fc32762e74eb79e5a747083ae
Chukwuma Nwaugha <nwaughac@gmail.com>
2024-10-30 11:24:15 +0000

    install anthropic sdk

---
+++ src/services/anthropic_client.py
@@ -0,0 +1,13 @@
+from anthropic import Anthropic, AsyncAnthropic
+
+from src.env_var import ANTHROPIC_API_KEY
+
+
+def get_anthropic():
+    """Return anthropic async client"""
+    return AsyncAnthropic(api_key=ANTHROPIC_API_KEY)
+
+
+def get_anthropic_sync():
+    """Return anthropic sync client"""
+    return Anthropic(api_key=ANTHROPIC_API_KEY)
