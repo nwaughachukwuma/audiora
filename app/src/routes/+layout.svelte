15b438b2a4e403518a570df2ed4cfdbd3c57fa91
Chukwuma Nwaugha <nwaughac@gmail.com>
2024-11-05 14:18:05 +0000

    Create basic layout content

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,1 +1,38 @@
-<slot />
+<script lang="ts" context="module">
+	import '../app.css';
+	import { page } from '$app/stores';
+	// import { browser } from '$app/environment';
+	// import { goto } from '$app/navigation';
+	// import { sessionStore } from '$lib/stores/session';
+	// import ChatInterface from '$lib/components/ChatInterface.svelte';
+	// import AudioInterface from '$lib/components/AudioInterface.svelte';
+
+	// $: userSpecification = $sessionStore.userSpecification;
+</script>
+
+<svelte:head>
+	<title>Audiora</title>
+</svelte:head>
+
+<div class="min-h-screen bg-background flex gap-x-2">
+	<aside class="shrink-0 h-screen pt-10 w-64 bg-gray-900 p-4">
+		<div class="text-sm text-muted-foreground p-3 rounded-lg bg-gray-700">
+			A VeedoAI project. (c) 2024
+		</div>
+
+		<div class="pt-2">
+			{#if $page.data.contentCategory}
+				<h3 class="mt-4 text-lg font-semibold">Content Category:</h3>
+				<p class="capitalize">{$page.data.contentCategory}</p>
+			{:else}
+				<p class="mt-4 text-sm text-muted-foreground">
+					Your preferences and audiocast metadata will appear here
+				</p>
+			{/if}
+		</div>
+	</aside>
+
+	<main class="container mx-auto px-6 pt-10 md:px-10">
+		<slot />
+	</main>
+</div>
--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,1 +1,25 @@
-<div>Welcome to Audiora</div>
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	// import { sessionStore } from '$lib/stores/session';
+	// import ChatInterface from '$lib/components/ChatInterface.svelte';
+	// import AudioInterface from '$lib/components/AudioInterface.svelte';
+
+	// $: userSpecification = $sessionStore.userSpecification;
+</script>
+
+<svelte:head>
+	<title>Audiora</title>
+</svelte:head>
+
+<div class="max-w-4xl mx-auto">
+	<h1 class="text-4xl font-bold mb-2">🎧 Audiora</h1>
+	<h2 class="text-2xl mb-8">Listen to anything, anytime, leveraging AI</h2>
+
+	<!-- {#if !userSpecification}
+	  <ChatInterface />
+	{:else}
+	  <AudioInterface />
+	{/if} -->
+</div>
