d25d4341e9dd43170e2b9fd753edc417b4ad045c
Chukwuma Nwaugha <nwaughac@gmail.com>
2024-11-11 14:27:44 +0000

    install redis for content caching

--- api/src/env_var.py
+++ api/src/env_var.py
@@ -14,5 +14,8 @@ ANTHROPIC_API_KEY = environ["ANTHROPIC_API_KEY"]
 GEMINI_API_KEY = environ["GEMINI_API_KEY"]
 ELEVENLABS_API_KEY = environ["ELEVENLABS_API_KEY"]
 
+REDIS_HOST = environ["REDIS_HOST"]
+REDIS_PASSWORD = environ["REDIS_PASSWORD"]
+
 APP_URL = environ.get("APP_URL", "http://localhost:8501")
 API_URL = environ["API_URL"]
