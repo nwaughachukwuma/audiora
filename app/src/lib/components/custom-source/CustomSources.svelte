32586d86abae77c2e3ba9b5f9934766e41a4c1a8
Chukwuma Nwaugha <nwaughac@gmail.com>
2024-11-20 19:06:36 +0000

    wip: add component for rendering pdf content

--- app/src/lib/components/custom-source/CustomSources.svelte
+++ app/src/lib/components/custom-source/CustomSources.svelte
@@ -1,28 +1,63 @@
-<script>
-	import * as Accordion from '../ui/accordion';
+<script lang="ts" context="module">
+	export type LinkSources = {
+		type: 'link';
+		url: string;
+	};
 
-	const customSources = [
+	export type CopyPasteSources = {
+		type: 'copy/paste';
+	};
+
+	export type CustomSources = (LinkSources | CopyPasteSources) & {
+		id: string;
+		title: string;
+		content_type: 'text/plain' | 'text/html' | 'application/pdf';
+		content: string;
+	};
+
+	const customSources: CustomSources[] = [
 		{
 			id: '1',
 			type: 'copy/paste',
+			content_type: 'text/html',
 			title: 'Custom Source 1',
 			content: 'AI-generated Source content goes here'
 		},
 		{
 			id: '2',
-			type: 'webpage',
 			title: 'Custom Source 2',
-			content: 'Custom Souce content goes here'
+			content_type: 'text/plain',
+			type: 'link',
+			content: 'Custom Souce content goes here',
+			url: 'https://www.google.com'
+		},
+		{
+			id: '3',
+			type: 'link',
+			content_type: 'application/pdf',
+			title: 'Custom Source 3',
+			content: 'Custom Souce content goes here',
+			url: 'https://www.wikipedia.org'
 		}
 	];
 </script>
 
-{#each customSources as source (source.id)}
+<script lang="ts">
+	import * as Accordion from '../ui/accordion';
+	import RenderPdfContent from './RenderPDFContent.svelte';
+	export let sources = customSources;
+</script>
+
+{#each sources as source (source.id)}
 	<Accordion.Item value={source.id} class="border-gray-800">
 		<Accordion.Trigger>{source.title}</Accordion.Trigger>
 		<Accordion.Content>
 			<div class="flex w-full flex-col gap-y-3 p-2 bg-gray-900/70 text-gray-300">
-				{source.content}
+				{#if source.content_type === 'application/pdf'}
+					<RenderPdfContent content={source.content} />
+				{:else}
+					{source.content}
+				{/if}
 			</div>
 		</Accordion.Content>
 	</Accordion.Item>
---
+++ app/src/lib/components/custom-source/RenderPDFContent.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let content: string;
+	$: paragraphs = content.split('\n').filter((p) => p.trim());
+</script>
+
+<article class="prose max-w-full flex flex-col gap-y-3">
+	{#each paragraphs as paragraph, idx (idx)}
+		{paragraph}
+	{/each}
+</article>
