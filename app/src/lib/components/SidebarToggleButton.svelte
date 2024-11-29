82395b2541d221e9d4937da0dd5ed6557448844d
Chukwuma Nwaugha <nwaughac@gmail.com>
2024-11-29 11:01:58 +0000

    abstract sidebar toggle button

---
+++ app/src/lib/components/SidebarToggleButton.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { Button } from './ui/button';
+	export let builders = undefined;
+</script>
+
+<Button {builders} variant="ghost" class="px-2" on:click>
+	<svg viewBox="0 0 24 24" fill="none" class="text-icon-secondary h-6">
+		<path
+			fill="currentColor"
+			fill-rule="evenodd"
+			clip-rule="evenodd"
+			d="M2 6C2 5.44772 2.44772 5 3 5H21C21.5523 5 22 5.44772 22 6C22 6.55228 21.5523 7 21 7H3C2.44772 7 2 6.55228 2 6ZM2 12C2 11.4477 2.44772 11 3 11H21C21.5523 11 22 11.4477 22 12C22 12.5523 21.5523 13 21 13H3C2.44772 13 2 12.5523 2 12ZM2 18C2 17.4477 2.44772 17 3 17H11C11.5523 17 12 17.4477 12 18C12 18.5523 11.5523 19 11 19H3C2.44772 19 2 18.5523 2 18Z"
+		></path>
+	</svg>
+</Button>
