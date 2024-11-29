ee7dd94c2b27e38fa4cf22513a4ed6fbbc648a5f
Chukwuma Nwaugha <nwaughac@gmail.com>
2024-11-29 11:05:05 +0000

    allow chatbox container to fill its parent's width

--- app/src/lib/components/ChatBoxContainer.svelte
+++ app/src/lib/components/ChatBoxContainer.svelte
@@ -8,16 +8,12 @@
 	export let disabled = false;
 </script>
 
-<div
-	class="fixed bg-background bottom-0 block w-full max-w-full max-sm:px-2 py-4 sm:max-w-xl lg:max-w-3xl"
->
+<div class="bg-background block w-full max-sm:px-2 py-4">
 	<div class="block w-full items-center">
 		<ChatBox {showIcon} disabled={loading || disabled} bind:searchTerm on:keypress>
 			<svelte:fragment slot="icon-right" let:disabled>
 				<ChatBoxIcon iconType="send" {loading} {disabled} {searchTerm} on:click />
 			</svelte:fragment>
 		</ChatBox>
-
-		<div class="h-6"></div>
 	</div>
 </div>
