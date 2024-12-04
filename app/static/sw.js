0e3b000655133b61464bce1d860050731bca81ef
Chukwuma Nwaugha <nwaughac@gmail.com>
2024-12-04 10:51:46 +0000

    remove other icon variants

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,16 +1,5 @@
 const CACHE_NAME = 'audiora-v1';
-const urlsToCache = [
-	'/',
-	'/manifest.json',
-	'/icons/icon-72x72.png',
-	'/icons/icon-96x96.png',
-	'/icons/icon-128x128.png',
-	'/icons/icon-144x144.png',
-	'/icons/icon-152x152.png',
-	'/icons/icon-192x192.png',
-	'/icons/icon-384x384.png',
-	'/icons/icon-512x512.png'
-];
+const urlsToCache = ['/', '/manifest.json', '/apple-touch-icon.png'];
 
 self.addEventListener('install', (event) => {
 	event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
