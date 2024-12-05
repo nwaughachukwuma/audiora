5fb138e7284e0d27bdd4552e8a6db3ba985868c3
Chukwuma Nwaugha <nwaughac@gmail.com>
2024-12-05 08:51:59 +0000

    build the ui workflow to automatically detect content category

--- app/src/lib/components/RenderCategorySelection.svelte
+++ app/src/lib/components/RenderCategorySelection.svelte
@@ -1,15 +1,54 @@
 <script lang="ts">
+	import { createEventDispatcher } from 'svelte';
 	import ChatListItem from './ChatListItem.svelte';
-	import SelectCategory from './SelectCategory.svelte';
+	import SelectCategory, { categories } from './SelectCategory.svelte';
+	import type { ContentCategory } from '@/utils/types';
+	import { env } from '@env';
+
 	export let content: string;
+
+	const dispatch = createEventDispatcher<{ detectedCategory: ContentCategory }>();
+
+	let detectingCategory = false;
+
+	$: getCategorySelection();
+
+	async function getCategorySelection() {
+		if (detectingCategory) return;
+		detectingCategory = true;
+
+		const response = await fetch(`${env.API_BASE_URL}/detect-category`, {
+			method: 'POST',
+			body: JSON.stringify({ content }),
+			headers: { 'Content-Type': 'application/json' }
+		})
+			.then<ContentCategory>((res) => {
+				if (res.ok) return res.json();
+				throw new Error(res.statusText);
+			})
+			.catch(() => void 0)
+			.finally(() => (detectingCategory = false));
+
+		if (response && categories.includes(response)) {
+			dispatch('detectedCategory', response);
+		}
+	}
 </script>
+
+<!-- Rename to get_category_selection.svelte -->
 
 <div class="flex flex-col gap-y-3 h-full">
 	<div class="flex flex-col gap-y-3 w-full">
 		<ChatListItem type="user" {content} />
 
-		<ChatListItem type="assistant" content="Please select your audiocast category" />
+		{#if detectingCategory}
+			<ChatListItem type="assistant" content="Auto-detecting content category..." loading />
+		{:else}
+			<ChatListItem type="assistant" content="Please select your audiocast category" />
+		{/if}
 	</div>
 
-	<SelectCategory on:selectCategory />
+	{#if !detectingCategory}
+		<SelectCategory on:selectCategory />
+	{/if}
 </div>
