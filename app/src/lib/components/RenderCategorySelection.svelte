0b23436b871b9d21a5153196bedf236e8f80756e
Chukwuma Nwaugha <nwaughac@gmail.com>
2024-12-05 09:56:42 +0000

    add improved workflow for selecting auto-detected contentcategory

--- app/src/lib/components/RenderCategorySelection.svelte
+++ app/src/lib/components/RenderCategorySelection.svelte
@@ -1,15 +1,31 @@
+<script lang="ts" context="module">
+	import type { ContentCategory } from '@/utils/types';
+	export const categoryArticleMap: Record<ContentCategory, string> = {
+		podcast: 'a podcast',
+		sermon: 'a sermon',
+		audiodrama: 'an audiodrama',
+		lecture: 'a lecture',
+		commentary: 'a commentary',
+		voicenote: 'a voicenote',
+		interview: 'an interview',
+		soundbite: 'a soundbite'
+	};
+</script>
+
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
+	import { ArrowRight } from 'lucide-svelte';
 	import ChatListItem from './ChatListItem.svelte';
 	import SelectCategory, { categories } from './SelectCategory.svelte';
-	import type { ContentCategory } from '@/utils/types';
 	import { env } from '@env';
+	import { Button } from './ui/button';
 
 	export let content: string;
 
-	const dispatch = createEventDispatcher<{ detectedCategory: ContentCategory }>();
+	const dispatch = createEventDispatcher<{ selectCategory: { value: ContentCategory } }>();
 
 	let detectingCategory = false;
+	let detectedCategory: ContentCategory | null = null;
 
 	$: getCategorySelection();
 
@@ -17,7 +33,7 @@
 		if (detectingCategory) return;
 		detectingCategory = true;
 
-		const response = await fetch(`${env.API_BASE_URL}/detect-category`, {
+		return fetch(`${env.API_BASE_URL}/detect-category`, {
 			method: 'POST',
 			body: JSON.stringify({ content }),
 			headers: { 'Content-Type': 'application/json' }
@@ -26,12 +42,9 @@
 				if (res.ok) return res.json();
 				throw new Error(res.statusText);
 			})
+			.then((res) => categories.includes(res) && (detectedCategory = res))
 			.catch(() => void 0)
 			.finally(() => (detectingCategory = false));
-
-		if (response && categories.includes(response)) {
-			dispatch('detectedCategory', response);
-		}
 	}
 </script>
 
@@ -47,6 +60,26 @@
 					Auto-detecting content category...Please wait
 				</span>
 			</ChatListItem>
+		{:else if detectedCategory}
+			{@const categoryWithArticle = `"${categoryArticleMap[detectedCategory]}"`}
+			{@const _detectedCategory = detectedCategory}
+			<p class="text-gray-300">
+				<ChatListItem
+					type="assistant"
+					content="I auto-detected you want {categoryWithArticle}. Press NEXT to continue if correct."
+				/>
+			</p>
+
+			<Button
+				variant="ghost"
+				class="text-base px-10 py-6 bg-gray-800 w-fit hover:bg-gray-700"
+				on:click={() => dispatch('selectCategory', { value: _detectedCategory })}
+			>
+				<span> Next </span>
+				<ArrowRight class="w-4 ml-1 inline" />
+			</Button>
+
+			<ChatListItem type="assistant" content="Else, select your audiocast category" />
 		{:else}
 			<ChatListItem type="assistant" content="Please select your audiocast category" />
 		{/if}
