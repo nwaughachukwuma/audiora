874b6044b9706720d065e79aa58ed062090c3eeb
Chukwuma Nwaugha <nwaughac@gmail.com>
2025-04-19 15:13:38 +0100

    Refactor GeminiClient to improve model handling and update requirements for Google GenAI

--- api/src/services/gemini_client.py
+++ api/src/services/gemini_client.py
@@ -2,8 +2,12 @@ from dataclasses import dataclass
 from typing import Any, Callable, Literal, Optional
 
 import google.generativeai as genai
+from google import genai as gemini_ai
+from google.genai import types
 
 from src.env_var import GEMINI_API_KEY
+
+ModelName = Literal["gemini-1.5-flash-8b", "gemini-1.5-flash", "gemini-1.5-pro", "gemini-2.0-flash"]
 
 
 def get_gemini():
@@ -11,7 +15,12 @@ def get_gemini():
     return genai
 
 
-ModelName = Literal["gemini-2.0-flash", "gemini-1.5-pro", "gemini-1.5-pro-latest"]
+class GeminiClient:
+    _client = gemini_ai.Client(api_key=GEMINI_API_KEY)
+    _models = _client.models
+    _types = types
+    _Content = types.Content
+    _Part = types.Part
 
 
 @dataclass
